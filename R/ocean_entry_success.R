2269070d6ff34847315ab4f2894f2beeafa7b5b8
ErinCain <ecain@flowwest.com>
2021-07-16 12:20:46 -0700

    Updates ocean entry success documenation

--- R/ocean_entry_success.R
+++ R/ocean_entry_success.R
@@ -1,17 +1,18 @@
 #' @title Ocean Entry Success
 #' @description Calculates the number of juveniles that survive entering the ocean
+#' @details See \code{\link{params}} for details on parameter sources
 #' @param migrants Variable representing the number of juveniles at golden gate bridge
 #' @param month Variable representing the current simulation month
 #' @param avg_ocean_transition_month Variable representing the average month juveniles transition to the ocean
-#' @param length Variable representing the fork lengths for each size classes. \href{https://dsm-docs.s3-us-west-2.amazonaws.com/Satterthwaite_et_al_2014.pdf}{Satterthwaite et al. (2014)}
-#' @param ..ocean_entry_success_int Intercept, source: Calibration (Varies by tributary )
-#' @param .month Coefficient for month variable, source: \href{https://dsm-docs.s3-us-west-2.amazonaws.com/Satterthwaite_et_al_2014.pdf}{Satterthwaite et al. (2014)}
+#' @param .ocean_entry_success_length Size related intercept representing the fork lengths for each size classes
+#' @param ..ocean_entry_success_int Intercept
+#' @param .ocean_entry_success_months Coefficient for month variable
 #' @source IP-117068
 #' @export
 ocean_entry_success <- function(migrants, month, avg_ocean_transition_month,
-                                .ocean_entry_success_length,
-                                ..ocean_entry_success_int = ..ocean_entry_success_int,
-                                .ocean_entry_success_months){
+                                .ocean_entry_success_length = lateFallRunDSM::params$.ocean_entry_success_length,
+                                ..ocean_entry_success_int = lateFallRunDSM::params$..ocean_entry_success_int,
+                                .ocean_entry_success_months = lateFallRunDSM::params$.ocean_entry_success_months){
 
   month_since <- ifelse(month <= avg_ocean_transition_month, 0, max(1, month - avg_ocean_transition_month))
 
