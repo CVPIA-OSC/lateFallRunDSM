61f80010e2312554759d8e9bc7f57ebed5303001
sadie <sgill@flowwest.com>
2021-08-24 13:28:55 -0700

    removes unused egg temp effect param

--- data-raw/model-parameters.R
+++ data-raw/model-parameters.R
@@ -54,7 +54,6 @@ params <- list(
   vernalis_temps = DSMtemperature::vernalis_temperature,
   prisoners_point_temps = DSMtemperature::prisoners_point_temperature,
   degree_days = DSMtemperature::degree_days,
-  mean_egg_temp_effect = DSMtemperature::egg_temperature_effect$fall_run,
   avg_temp = DSMtemperature::stream_temperature,
   avg_temp_delta = DSMtemperature::delta_temperature,
   migratory_temperature_proportion_over_20 = DSMtemperature::migratory_temperature_proportion_over_20,
