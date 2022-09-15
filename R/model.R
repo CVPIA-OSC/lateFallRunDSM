ec761fcde966221998504ab384d83dd828408fa1
MaddeeRubenson <rubenson.m@gmail.com>
2022-09-15 12:05:41 -0700

    adding survival analysis WIP

--- R/model.R
+++ R/model.R
@@ -16,7 +16,10 @@
 #' @export
 late_fall_run_model <- function(scenario = NULL, mode = c("seed", "simulate", "calibrate"),
                                 seeds = NULL, ..params = lateFallRunDSM::params,
-                                stochastic = FALSE){
+                                stochastic = FALSE,
+                                which_surv = c("egg_to_fry", "juv_rear", "juv_migratory"),
+                                location_surv = "Upper Sacramento River",
+                                month_surv = 1){
   
   mode <- match.arg(mode)
   
@@ -135,6 +138,11 @@ late_fall_run_model <- function(scenario = NULL, mode = c("seed", "simulate", "c
       .scour = ..params$.surv_egg_to_fry_scour
     )
     
+    # egg to fry sensitivity analysis
+    if (mode == "simulate" & !is.na(which_surv) & which_surv == "egg_to_fry") {
+      egg_to_fry_surv[location_surv] <- min(egg_to_fry_surv[location_surv] * 1.2, 1) 
+    }
+    
     next_year <- year + 1
     min_spawn_habitat <- apply(..params$spawning_habitat[ , c(12, 1, 2), c(year, next_year, next_year)], 1, min)
     
@@ -233,6 +241,30 @@ late_fall_run_model <- function(scenario = NULL, mode = c("seed", "simulate", "c
                                                min_survival_rate = ..params$min_survival_rate,
                                                stochastic = stochastic)
       
+      # juvenile rearing sensitivity analysis
+      if (mode == "simulate" & !is.na(which_surv) &  which_surv == "juv_rear"  & month == month_surv) {
+        if(location_surv %in% c("Upper Sacramento River", "Lower-mid Sacramento River", 
+                                "Lower Sacramento River", "Upper-mid Sacramento River", 
+                                "Battle Creek")) {
+          index_position <- which(winterRunDSM::watershed_labels == location_surv)
+          rearing_survival$inchannel[index_position, ] <- pmin(rearing_survival$inchannel[index_position, ] * 1.2, 1)
+          rearing_survival$floodplain[index_position, ] <- pmin(rearing_survival$floodplain[index_position, ] * 1.2, 1)
+        }
+        
+        if (location_surv == "Sutter Bypass") {
+          rearing_survival$sutter <- pmin(rearing_survival$sutter * 1.2, 1)
+        }
+        
+        if (location_surv == "Yolo Bypass") {
+          rearing_survival$yolo <- pmin(rearing_survival$yolo * 1.2, 1)
+        }
+        
+        if (location_surv %in% c("North Delta", "South Delta")) {
+          rearing_survival$delta[location_surv, ] <- pmin(rearing_survival$delta[location_surv, ] * 1.2, 1)
+        }
+      }
+      
+      
       migratory_survival <- get_migratory_survival(year, month,
                                                    cc_gates_prop_days_closed = ..params$cc_gates_prop_days_closed,
                                                    freeport_flows = ..params$freeport_flows,
@@ -252,6 +284,38 @@ late_fall_run_model <- function(scenario = NULL, mode = c("seed", "simulate", "c
                                                    .surv_juv_outmigration_san_joaquin_large = ..params$.surv_juv_outmigration_san_joaquin_large, 
                                                    min_survival_rate = ..params$min_survival_rate,
                                                    stochastic = stochastic)
+      
+      # juvenile migration sensitivity analysis
+      if (mode == "simulate" & !is.na(which_surv) & which_surv == "juv_migratory" & month == month_surv) {
+        
+        if (location_surv == "Upper-mid Sacramento River") {
+          migratory_survival$uppermid_sac = pmin(migratory_survival$uppermid_sac* 1.2, 1)
+        }
+        
+        if (location_surv == "Lower-mid Sacramento River") {
+          migratory_survival$lowermid_sac = pmin(migratory_survival$lowermid_sac* 1.2, 1)
+        }
+        
+        if (location_surv == "Lower Sacramento River") {
+          migratory_survival$lower_sac = pmin(migratory_survival$lower_sac* 1.2, 1)
+        }
+        
+        if (location_surv == "Sutter Bypass") {
+          migratory_survival$sutter = pmin(migratory_survival$sutter * 1.2, 1)
+        }
+        
+        if (location_surv == "Yolo Bypass") {
+          migratory_survival$yolo = pmin(migratory_survival$yolo * 1.2, 1)
+        }
+        
+        if (location_surv == "Delta") {
+          migratory_survival$delta = pmin(migratory_survival$delta * 1.2, 1)
+        }
+        
+        if (location_surv == "Bay Delta") {
+          migratory_survival$bay_delta = min(migratory_survival$bay_delta * 1.2, 1)
+        }
+      }
       
       fish_1 <- juvenile_month_dynamic(hypothesis = 1, fish_1, year = year, month = month, 
                                        rearing_survival = rearing_survival, 
---
+++ analysis/survival_sensitivity_analysis.R
@@ -0,0 +1,124 @@
+library(dplyr)
+
+set.seed(123119)
+sensi_seeds <- lateFallRunDSM::late_fall_run_model(mode = "seed")
+
+
+sensitivity_latefall_run_model <- function(scenario, scenarios, sensi_seeds) {
+  
+  which_surv = scenarios[scenario, ]$which_surv
+  location_surv = scenarios[scenario, ]$location_surv
+  month_surv = scenarios[scenario, ]$month_surv
+  
+  model_results <- lateFallRunDSM::late_fall_run_model(mode = "simulate",
+                                                  seeds = sensi_seeds, 
+                                                  which_surv = which_surv,
+                                                  location_surv = location_surv,
+                                                  month_surv = month_surv)
+  
+  output <- dplyr::as_tibble((model_results$spawners * model_results$proportion_natural)[c(1, 3), ]) |> 
+    dplyr::mutate(location = c("Upper Sacramento River", "Battle Creek"),
+                  survival_target = which_surv, 
+                  location_target = location_surv, 
+                  month_target = month_surv,
+                  id = scenario) |> 
+    dplyr::select(id, location, survival_target, location_target, month_target, `1`:`20`)
+  
+  return(output)
+  
+}
+
+# run scenarios in parallel -------
+library(tictoc) # measure time to run
+library(parallel)
+library(doParallel)
+
+# scenarios to evaluate 
+scenarios1 <- expand.grid(location_surv = lateFallRunDSM::watershed_labels[c(16, 21, 24, 17, 22, 3)], 
+                          month_surv = c(9:12, 1:5),
+                          which_surv = c("juv_rear", "juv_migratory")) 
+
+scenarios2  <- expand.grid(location_surv = c("Upper Sacramento River", "South Delta"),
+                           month_surv = c(9:12, 1:5),
+                           which_surv = "juv_rear") 
+
+scenarios3  <- expand.grid(location_surv = c("Delta","Bay Delta"), 
+                           month_surv = c(9:12, 1:5),
+                           which_surv = "juv_migratory")
+
+scenarios4  <- expand.grid(location_surv = c("Upper Sacramento River","Battle Creek"),
+                           month_surv = NA,
+                           which_surv = "egg_to_fry")
+
+scenarios5  <- expand.grid(location_surv = "North Delta",
+                           month_surv = c(9:12, 1:5),
+                           which_surv = "juv_rear") 
+
+# set up for running function in parallel
+no_cores <- detectCores(logical = TRUE)
+cl <- makeCluster(no_cores - 1)
+registerDoParallel(cl)
+
+clusterExport(cl, list("sensitivity_latefall_run_model", "sensi_seeds", 
+                       "scenarios1", "scenarios2", "scenarios3", "scenarios4", "scenarios5"))
+
+
+tic("parallel 1")
+scenario_results_list1 <- parLapply(cl, 1:nrow(scenarios1),
+                                    fun = function(scenario) {
+                                      sensitivity_latefall_run_model(scenario, scenarios1, sensi_seeds)
+                                    })
+toc()
+
+tic("parallel 2")
+scenario_results_list2 <- parLapply(cl, 1:nrow(scenarios2),
+                                    fun = function(scenario) {
+                                      sensitivity_latefall_run_model(scenario, scenarios2, sensi_seeds)
+                                    })
+toc()
+
+tic("parallel 3")
+scenario_results_list3 <- parLapply(cl, 1:nrow(scenarios3),
+                                    fun = function(scenario) {
+                                      sensitivity_latefall_run_model(scenario, scenarios3, sensi_seeds)
+                                    })
+toc()
+
+tic("parallel 4")
+scenario_results_list4 <- parLapply(cl, 1:nrow(scenarios4),
+                                    fun = function(scenario) {
+                                      sensitivity_latefall_run_model(scenario, scenarios4, sensi_seeds)
+                                    })
+toc()
+
+tic("parallel 5")
+scenario_results_list5 <- parLapply(cl, 1:nrow(scenarios5),
+                                    fun = function(scenario) {
+                                      sensitivity_latefall_run_model(scenario, scenarios5, sensi_seeds)
+                                    })
+toc()
+
+# combine into one 
+r1 <- scenario_results_list1 |> dplyr::bind_rows()
+r2 <- scenario_results_list2 |> dplyr::bind_rows() |> dplyr::mutate(id = id + max(r1$id))
+r3 <- scenario_results_list3 |> dplyr::bind_rows() |> dplyr::mutate(id = id + max(r2$id))
+r4 <- scenario_results_list4 |> dplyr::bind_rows() |> dplyr::mutate(id = id + max(r3$id))
+r5 <- scenario_results_list5 |> dplyr::bind_rows() |> dplyr::mutate(id = id + max(r4$id))
+
+# do nothing
+model_results <- lateFallRunDSM::late_fall_run_model(mode = "simulate",
+                                                seeds = sensi_seeds, 
+                                                which_surv = NA,
+                                                location_surv = NA,
+                                                month_surv = NA)
+
+do_nothing <- dplyr::as_tibble((model_results$spawners * model_results$proportion_natural)[c(1, 3), ]) |> 
+  dplyr::mutate(location = c("Upper Sacramento River", "Battle Creek"),
+                survival_target = NA, 
+                location_target = NA, 
+                month_target = NA,
+                id = max(r5$id) + 1) |> 
+  dplyr::select(id, location, survival_target, location_target, month_target, `1`:`20`)
+
+results <- dplyr::bind_rows(r1, r2, r3, r4, r5, do_nothing)
+#write_csv(results, "analysis/late_fall_run_survival_sensi_model_ouput.csv")
